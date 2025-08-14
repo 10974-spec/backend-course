787e7f0b5555c56d544ae07ef165dffcdfe910e9
10974-spec <emmanuelnyakoe13@gmail.com>
2025-08-14 22:17:05 +0300

    connecting auth routes with auth controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,15 @@
 import { Router } from "express";
-
+import { signOut, signUp } from "../controllers/auth.controller.js";
+import { signIn } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 
-authRouter.get('/sign-up', (req, res) => {
-  res.send({ title: "sign-up" });
-});
 
-authRouter.get('/sign-in', (req, res) => {
-  res.send({ title: "sign-in" });
-});
+authRouter.get('/sign-up', signUp);
 
-authRouter.get('/sign-out', (req, res) => {
-  res.send({ title: "sign-out" });
-});
+authRouter.get('/sign-in', signIn);
+
+authRouter.get('/sign-out', signOut);
 
 export default authRouter;
