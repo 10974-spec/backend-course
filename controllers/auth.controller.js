133661ca7732a54e3963c7853891d9f606d21a5f
10974-spec <emmanuelnyakoe13@gmail.com>
2025-08-15 23:42:25 +0300

    commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -100,6 +100,7 @@ export const signIn = async (req, res, next) => {
 
 export const signOut = async (req, res, next) => {
   try {
+    
   } catch (error) {
     next(error);
   }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,8 +6,8 @@ const authRouter = Router();
 
 
 
-authRouter.get('/sign-up', signUp);
-authRouter.get('/sign-in', signIn);
-authRouter.get('/sign-out', signOut);
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
